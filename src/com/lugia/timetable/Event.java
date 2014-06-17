d0fe28c24847b4b78adc685d7106d6bfcddf5b72
Lugia0529 <Lugia1216@gmail.com>
2014-06-17 10:43:09 +0800

    Update copyright license

--- src/com/lugia/timetable/Event.java
+++ src/com/lugia/timetable/Event.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Lugia Programming Team
+ * Copyright (c) 2014 Lugia Programming Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
